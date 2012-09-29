8618f148398909f3d1cf4ddcad5f864a8b5cb116
Simon Cropp <simon.cropp@gmail.com>
2012-09-29 23:16:22 +1000

    make nuget package be project level

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("AsyncErrorHandler")]
 [assembly: AssemblyProduct("AsyncErrorHandler")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
