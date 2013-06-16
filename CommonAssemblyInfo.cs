f815b07aadf81cb2aec19c11b9ce62c4777a58e2
Simon Cropp <simon.cropp@gmail.com>
2013-06-16 18:53:12 +1000

    bump vesion

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("AsyncErrorHandler")]
 [assembly: AssemblyProduct("AsyncErrorHandler")]
-[assembly: AssemblyVersion("0.4.1.3")]
-[assembly: AssemblyFileVersion("0.4.1.3")]
+[assembly: AssemblyVersion("0.4.1.4")]
+[assembly: AssemblyFileVersion("0.4.1.4")]
