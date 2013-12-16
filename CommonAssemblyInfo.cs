206b0900dc23eb6f657a621ef449514c49e6e7bf
Simon Cropp <simon.cropp@gmail.com>
2013-12-16 20:31:46 +1100

    make nuget a dev time package

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("AsyncErrorHandler")]
 [assembly: AssemblyProduct("AsyncErrorHandler")]
-[assembly: AssemblyVersion("1.0.1")]
-[assembly: AssemblyFileVersion("1.0.1")]
+[assembly: AssemblyVersion("1.0.2")]
+[assembly: AssemblyFileVersion("1.0.2")]
